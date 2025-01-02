0f56fdd2d3c779ef50a1771d2c784bd2536cdbd1
mohanpola17 <mohanpola@mohans-MacBook-Air.local>
2025-01-02 18:08:17 +0530

    socket url updated

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,6 +1,7 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
 import mongoose from "mongoose";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const sendMessage = async(req,res) => {
      try {
@@ -28,14 +29,18 @@ export const sendMessage = async(req,res) => {
             conversation.messages.push(newMessage._id);
         }
         
-        //SOCKET IO FUNCTIONALITY WILL GO HERE
-
-
         // await conversation.save();
         // await newMessage.save();
 
         //this will run in parallel
         await Promise.all([conversation.save(),newMessage.save()]);
+
+        //SOCKET IO FUNCTIONALITY WILL GO HERE
+        const receiverSocketId = getReceiverSocketId(receiverId);
+        if(receiverSocketId) {
+            //io.to(<socket_id>).emit() used to send events to specific client
+            io.to(receiverSocketId).emit("newMessage",newMessage)
+        }
 
         res.status(201).json(newMessage);
      } catch (error) {
--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -1,30 +1,44 @@
-import { createContext, useEffect, useState} from "react";
-import { useAuthContext } from "./AuthContext";
+import { createContext, useContext, useEffect, useState} from "react";
 import  io  from "socket.io-client";
+import { useAuthContext } from "./authContext"
 
-export const SocketContext = createContext();
+
+const SocketContext = createContext();
+
+export const useSocketContext = () => {
+    return useContext(SocketContext);
+};
 
 export const SocketContextProvider = ({ children }) => {
     const [socket,setSocket] = useState(null);
     const [onlineUsers,setOnlineUsers] = useState([]);
     const {authUser} = useAuthContext();
 
-    console.log("auth");
+ 
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:4000");
+            const socket = io("http://localhost:4000",{
+                query:{
+                    userId: authUser._id,
+                }
+            });
 
             setSocket(socket);
 
+            socket.on("getOnlineUsers",(users) => {
+                setOnlineUsers(users);
+            })
+
             return () => socket.close();
-        } else {
+        } else 
+        {
             if(socket) {
                 socket.close();
                 setSocket(null);
             }
         }
-    },[]);
+    },[authUser]);
     
-    return <SocketContext.Provider value={{socket,onlineUsers}}></SocketContext.Provider>;
+    return (<SocketContext.Provider value={{socket,onlineUsers}}>{children}</SocketContext.Provider>);
 };
