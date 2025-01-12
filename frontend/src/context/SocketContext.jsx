e53092ad82726aa229c60104d6c3fd5c6537d7ba
mohanpola17 <mohanpola@mohans-MacBook-Air.local>
2025-01-12 15:01:44 +0530

    fixed socket issue

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -18,7 +18,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if(authUser) {
-            const socket = io("http://localhost:4000",{
+            const socket = io("https://proconnect-sc3e.onrender.com",{
                 query:{
                     userId: authUser._id,
                 }
