43a8443030f447807190159d1eeb16eba7bcf1bd
mohanpola17 <mohanpola@mohans-MacBook-Air.local>
2025-01-12 14:55:54 +0530

    fixed socket issue

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,8 +7,9 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server,{
     cors:{
-        origin:["http://localhost:4000","https://proconnect-sc3e.onrender.com/"],
-        methods:["GET","POST"]
+        origin:["https://proconnect-sc3e.onrender.com"],
+        methods:["GET","POST"],
+        credentials: true
     }
 });
 
